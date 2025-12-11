fdaca052c548210f94f3f6150eb5c900aa351247
Nihal <nihallaskar888@gmail.com>
2025-12-12 02:40:32 +0600

    added role changing functionalities

---
+++ src/Components/Dashboard/UserTable.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+const UserTable = ({ user, index }) => {
+    const handleRoleChange = async (e) => {
+        const role = e.target.value
+        console.log(role);
+         try{
+            await axios.patch(`${import.meta.env.VITE_DOMAIN}/update-role`,{email: user?.email,role: role })
+
+        }
+         catch{
+            console.log('jkshdf');
+         }
+    }
+    return (
+            <tbody>
+                <tr>
+                    <th>{index + 1}</th>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <form onChange={handleRoleChange} action="">
+                            <select defaultValue="Server location" className="select select-neutral">
+                                <option disabled={true}>{user.role}</option>
+                                <option value='Manager'>Manager</option>
+                                <option value='Borrower'>Borrower</option>
+                                <option value='Admin'>Admin</option>
+                            </select>
+                        </form>
+                    </td>
+                    <td className='flex gap-2'>
+                        <button className='btn bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white' >Update</button>
+                    </td>
+                </tr>
+            </tbody>
+    );
+};
+
+export default UserTable;  
--- src/Pages/Dashboard/AdminUsers.jsx
+++ src/Pages/Dashboard/AdminUsers.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import UserTable from '../../Components/Dashboard/UserTable';
 
 const AdminUsers = () => {
     const [users, setusers] = useState([])
@@ -15,10 +16,12 @@ const AdminUsers = () => {
     }, [])
     if (loading) return <div>loading...</div>
     console.log(users);
+
+
     return (
         <div>
             <div className="overflow-x-auto rounded-box border border-base-content/5 bg-transparent">
-                <table className="table">
+                <table className="table border-2">
                     {/* head */}
                     <thead>
                         <tr>
@@ -29,22 +32,14 @@ const AdminUsers = () => {
                             <th>Actions</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    
                         {/* row 1 */}
                         {
                             users.map((user, index) =>
-                                <tr>
-                                    <th>{index + 1}</th>
-                                    <td>{user.name}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.role}</td>
-                                    <td className='flex gap-2'>
-                                        <button className='btn bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white' >Update</button>
-                                    </td>
-                                </tr>
+                                <UserTable user={user} index={index}/>
                             )
                         }
-                    </tbody>
+                
                 </table>
             </div>
         </div>
