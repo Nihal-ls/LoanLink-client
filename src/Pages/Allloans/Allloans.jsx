c55c6877b1dffc6d981ae22d6be1b149d0cc8812
Nihal <nihallaskar888@gmail.com>
2026-01-11 23:50:47 +0600

    chanegd in new domain

--- src/Pages/Allloans/Allloans.jsx
+++ src/Pages/Allloans/Allloans.jsx
@@ -19,7 +19,7 @@ const Allloans = () => {
             try {
                 setLoading(true);
 
-                const res = await axios.get('http://localhost:5000/Allloans', {
+                const res = await axios.get(`${import.meta.env.VITE_DOMAIN}/Allloans`, {
                     params: {
                         search: searchTerm || undefined,
                         interest: interest || undefined,
